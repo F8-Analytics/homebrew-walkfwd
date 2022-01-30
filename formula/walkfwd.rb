9d60b995533d97466b98e423c61d731aa85e2d49
F8-Analytics <57007013+F8-Analytics@users.noreply.github.com>
2022-01-29 23:08:44 -0500

    Update walkfwd.rb

--- formula/walkfwd.rb
+++ formula/walkfwd.rb
@@ -1,8 +1,11 @@
-class Walkfwd < Formula
+require "formula"
+
+class Walk < Formula
   desc "walkfwd CSV File Normalization Tool v0.1.0"
-  homepage "https://github.com/F8-Analytics/walkfwd"
-  url "https://github.com/F8-Analytics/walkfwd/walkfwd-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy"
-  head "https://github.com/F8-Analytics/walkfwd.git"
+  homepage "https://github.com/F8-Analytics/homebrew-walkfwd"
+  url "https://github.com/F8-Analytics/homebrew-walkfwd/walkfwd-darwin-amd64.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  sha256 "dfff81379dba66c83d9706800798dca66c692c8a22499261cc8eca765b571af7"
+  head "https://github.com/F8-Analytics/homebrew-walkfwd.git"
 
   def install
     bin.install "walkfwd"
@@ -10,6 +13,6 @@ class Walkfwd < Formula
 
   # Homebrew requires tests.
   test do
-    assert_match "walkfwd version 0.1.0", shell_output("#{bin}/0.1.0 -v", 2)
+    assert_match "walkfwd version 0.1.0", shell_output("#{bin}/walkfwd -v", 2)
   end
 end
