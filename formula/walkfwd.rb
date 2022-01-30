255195fa8817508ded4690f00ce90c886df77652
F8-Analytics <57007013+F8-Analytics@users.noreply.github.com>
2022-01-30 00:13:13 -0500

    Update walkfwd.rb

--- formula/walkfwd.rb
+++ formula/walkfwd.rb
@@ -1,7 +1,7 @@
 class Walkfwd < Formula
   desc "walkfwd CSV File Normalization Tool v0.1.0"
   homepage "https://github.com/F8-Analytics/homebrew-walkfw"
-  url "https://github.com/F8-Analytics/homebrew-walkfwd/releases/download/v0.1.0/walkfwd-darwin-amd$"
+  url "https://github.com/F8-Analytics/homebrew-walkfwd/releases/download/v0.1.0/walkfwd-darwin-amd64.tar.gz"
   sha256 "dfff81379dba66c83d9706800798dca66c692c8a22499261cc8eca765b571af7"
   head "https://github.com/F8-Analytics/homebrew-walkfwd.git"
   license "MIT"
