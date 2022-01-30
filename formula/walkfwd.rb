a5401c457d2c59852045e90b1bafe184a1b5804a
F8-Analytics <57007013+F8-Analytics@users.noreply.github.com>
2022-01-30 00:08:10 -0500

    Update walkfwd.rb

--- formula/walkfwd.rb
+++ formula/walkfwd.rb
@@ -1,17 +1,17 @@
 class HomebrewWalkfwd < Formula
-  desc "walkfwd CSV File Normalization Tool v0.1.0""
-  homepage ""https://github.com/F8-Analytics/homebrew-walkfwd"
+  desc "walkfwd CSV File Normalization Tool v0.1.0"
+  homepage "https://github.com/F8-Analytics/homebrew-walkfw"
   url "https://github.com/F8-Analytics/homebrew-walkfwd/releases/download/v0.1.0/walkfwd-darwin-amd$
   sha256 "dfff81379dba66c83d9706800798dca66c692c8a22499261cc8eca765b571af7"
-  license ""
+  license "MIT"
 
-  # depends_on "cmake" => :build
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    # https://rubydoc.brew.sh/Formula.html#std_configure_args-instance_method
-    system "./configure", *std_configure_args, "--disable-silent-rules"
-    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    bin.install "walkfwd"
   end
+
+  test do
+    assert_match "walkfwd version 0.1.0", shell_output("#{bin}/walkfwd -v", 2)
+  end
+end
 
